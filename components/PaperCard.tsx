9edabd4e5f7701cd56a6a6dec9b18385478f8e6d
Deepak Solanki <deepak@example.com>
2026-06-27 14:40:51 +0530

    UI cards: remove bookmark icon button from paper cards as requested

--- components/PaperCard.tsx
+++ components/PaperCard.tsx
@@ -62,12 +62,6 @@ export default function PaperCard({ paper }: { paper: any }) {
         })}
       </div>
 
-      {/* Bookmark button */}
-      <button className="card-bookmark" aria-label="Bookmark">
-        <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-          <path d="M19 21l-7-5-7 5V5a2 2 0 012-2h10a2 2 0 012 2z"/>
-        </svg>
-      </button>
     </article>
   );
 }
