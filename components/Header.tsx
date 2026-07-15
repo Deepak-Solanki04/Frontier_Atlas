666803e492df192bcff455e24f29a8b21379097f
Deepak Solanki <deepak@example.com>
2026-07-16 01:17:47 +0530

    style(header): reduce logo scale by exactly 25% to scale-[1.125] and xl:scale-[1.16]

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,14 +53,14 @@ export default function Header() {
         </button>
         <Link
           href="/"
-          className="flex items-center justify-center xl:justify-start cursor-pointer absolute left-1/2 -translate-x-1/2 xl:relative xl:left-auto xl:-translate-x-0 w-[240px] sm:w-[280px] xl:w-[320px] h-14 xl:h-16"
+          className="flex items-center justify-center xl:justify-start cursor-pointer absolute left-1/2 -translate-x-1/2 xl:relative xl:left-auto xl:-translate-x-0 w-[200px] sm:w-[230px] xl:w-[260px] h-12 xl:h-14"
         >
           <Image
             alt="Frontier Atlas"
             src="/logo.png"
             fill
-            className="object-contain object-center xl:object-left scale-[1.5] xl:scale-[1.55] origin-center xl:origin-left transition-transform"
-            sizes="(max-width: 1280px) 280px, 320px"
+            className="object-contain object-center xl:object-left scale-[1.125] xl:scale-[1.16] origin-center xl:origin-left transition-transform"
+            sizes="(max-width: 1280px) 230px, 260px"
             priority
           />
         </Link>
